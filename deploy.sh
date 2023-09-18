4cc954f7558b1b27963c2afeeaf84421e8d1d722
Hemanth B <hemanth22hemu@gmail.com>
2023-09-18 19:39:24 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+{
+  "environments": {
+    "UAT": [
+      {
+        "hostname": "uat-kubecentos,
+        "ip_address": "193.16.16.11",
+        "deployment_directory": "/home/vagrant/apps/",
+        "backup_directory": "/home/vagrant/backup/"
+      }
+    ],
+    "PROD": [
+      {
+        "hostname": "kubecentos",
+        "ip_address": "193.16.16.11",
+        "deployment_directory": "/home/vagrant/apps/",
+        "backup_directory": "/home/vagrant/backup/"
+      }
+    ]
+  }
+}
